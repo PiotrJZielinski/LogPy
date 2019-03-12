ac1fa6229fa14fee02bb9fa8add9da2cb94a54ad
PiotrJZielinski <piotrekzie100@gmail.com>
2019-03-12 12:20:20 +0100

    Fix setup file

--- setup.py
+++ setup.py
@@ -4,22 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyTransdec",
-    version="0.0.2",
-    author="Piotr Zielinski",
+    name="LogPy",
+    version="0.0.1",
+    author="Piotr Zielinski, Dominik Wasiolka",
     author_email="piotr_zielinski@g.pl",
-    description="Transdec environment https://github.com/PiotrJZielinski/TransdecEnvironment controlling with Python",
+    description="Versatile Python logger",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/PiotrJZielinski/PyTransdec",
+    url="https://github.com/PiotrJZielinski/LogPy",
     packages=setuptools.find_packages(),
-    install_requires=[
-        'numpy',
-        'pandas',
-        'tqdm',
-        'opencv-python',
-        'mlagents'
-    ],
+    install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
